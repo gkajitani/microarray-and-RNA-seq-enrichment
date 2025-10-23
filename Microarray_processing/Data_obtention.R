ac31b6ab99ae29e03f50f84d2956643bfc8ab04e
gkajitani <92863815+gkajitani@users.noreply.github.com>
2025-10-23 15:13:56 +0200

    Create Data_obtention.R

---
+++ Microarray_processing/Data_obtention.R
@@ -0,0 +1,76 @@
+############################################################################################
+# Example of an R code for obtaining differential expression data from GEO microarray data #
+############################################################################################
+
+# Version info: R 4.2.2, Biobase 2.58.0, GEOquery 2.66.0, limma 3.54.0
+################################################################
+#   Differential expression analysis with limma
+library(GEOquery)
+library(limma)
+library(umap)
+
+setwd("~/Desktop/CS_Paper/Todos_datasets_originais/Updated_microarray")
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE62194", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL6885", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# Note: CS samples should be marked as "1", control as "0".
+
+# group membership for all samples
+gsms <- paste0("XXXXXXXXXXXXXXXXXXX000000XXXXXXXXXXXXXXXXXXXXXXXXX",
+               "XXXXXXXXXX111XXXXXXXX")
+sml <- strsplit(gsms, split="")[[1]]
+
+# filter out excluded samples (marked as "X")
+sel <- which(sml != "X")
+sml <- sml[sel]
+gset <- gset[ ,sel]
+
+# log2 transformation
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# assign samples to groups and set up design matrix
+gs <- factor(sml)
+groups <- make.names(c("csb_adult","csb_young"))
+levels(gs) <- groups
+gset$group <- gs
+design <- model.matrix(~group + 0, gset)
+colnames(design) <- levels(gs)
+
+gset <- gset[complete.cases(exprs(gset)), ] # skip missing values
+
+fit <- lmFit(gset, design)  # fit linear model
+
+# set up contrasts of interest and recalculate model coefficients
+cts <- paste(groups[1], groups[2], sep="-")
+cont.matrix <- makeContrasts(contrasts=cts, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+
+# compute statistics and table of significant genes
+fit2 <- eBayes(fit2, 0.01)
+tT <- topTable(fit2, adjust="fdr", sort.by="B", number=2500000000)
+
+# Note: Some datasets have different colnames
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","logFC","Gene.symbol"))
+
+write.csv(tT, "Csb_adult_young.csv")
+
+write.table(tT, file=stdout(), row.names=F, sep="\t")
+
+# Visualize and quality control test results.
+# Build histogram of P-values for all genes. Normal test
+# assumption is that most genes are not differentially expressed.
+tT2 <- topTable(fit2, adjust="fdr", sort.by="B", number=Inf)
+hist(tT2$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+     ylab = "Number of genes", main = "P-adj value distribution")
